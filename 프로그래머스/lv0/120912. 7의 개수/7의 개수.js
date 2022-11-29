578bdbe07e1d68dc8c43887845af105cba13defc
hyunhoh <61953759+hyunhoh@users.noreply.github.com>
2022-11-30 06:00:15 +0900

    [level 0] Title: 7의 개수, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,3 +1,3 @@
 function solution(array) {
-    return array.map(ele => String(ele)).join('').split('').filter(ele => ele === '7').length;
+    return array.join('').split('').filter(ele => ele === '7').length;
 }
