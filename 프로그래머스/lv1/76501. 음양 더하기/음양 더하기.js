d8eb0c9c3ddaf0a20115fe943861cef56f6de2e6
hyunhoh <61953759+hyunhoh@users.noreply.github.com>
2022-12-02 20:00:05 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,3 +1,3 @@
 function solution(absolutes, signs) {
-    return absolutes.reduce((acc, cur, index) => signs[index] ? acc + cur : acc - cur,0)
+    return absolutes.reduce((acc, cur, index) => signs[index] ? acc + cur : acc - cur, 0);
 }
