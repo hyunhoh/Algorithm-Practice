ef683fa0786fe61f9a67298ca26b68accdf56dde
hyunhoh <61953759+hyunhoh@users.noreply.github.com>
2022-12-05 03:15:56 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,3 +1,3 @@
 function solution(s) {
-    return s.length % 2 ? (s[Math.floor(s.length / 2)]) : s.slice(s.length / 2 - 1, s.length / 2 + 1);
+    return s.length % 2 ? s[Math.floor(s.length / 2)] : s.slice(s.length / 2 - 1, s.length / 2 + 1);
 }
