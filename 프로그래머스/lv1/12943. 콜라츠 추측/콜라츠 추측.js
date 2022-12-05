a2d7c54ba31e932ece7a182cdd6c1658566d2898
hyunhoh <61953759+hyunhoh@users.noreply.github.com>
2022-12-05 21:50:32 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,10 @@
+function solution(num) {
+    let count = 0;
+    
+    while(num > 1) {
+        num % 2 ? num = num * 3 + 1 : num = num / 2;
+        count++;
+    }
+    
+    return count > 500 ? -1 : count;
+}
