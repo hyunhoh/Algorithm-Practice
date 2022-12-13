a45097b56f6923700e4b695ac6ae223d1eb32419
hyunhoh <61953759+hyunhoh@users.noreply.github.com>
2022-12-14 03:03:36 +0900

    [level 0] Title: 가까운 수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,4 +1,3 @@
 function solution(array, n) {
-    return array.reduce((acc, cur) => Math.abs(acc - n) < Math.abs(cur - n) ? acc :
-                        Math.abs(acc - n) === Math.abs(cur - n) && acc < n ? acc : cur);
+    return array.reduce((acc, cur) => Math.abs(acc - n) < Math.abs(cur - n) ? acc : Math.abs(acc - n) === Math.abs(cur - n) ? Math.min(acc, cur) : cur);
 }
