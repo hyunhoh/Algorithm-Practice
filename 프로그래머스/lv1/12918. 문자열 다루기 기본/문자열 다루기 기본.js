1eea2c2bb50c138b5c0b749548dae77cc179381e
hyunhoh <61953759+hyunhoh@users.noreply.github.com>
2022-12-27 23:18:51 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,4 +1,10 @@
 function solution(s) {
+    // return (s.length === 4 || s.length === 6) && !Number.isNaN(Number(s)) ? true : false;
+    // 위와 같이 작성하면 테스트 11번에서 걸리게된다.
+    // 그래서 이유를 살펴보니 16진수 형태의 문자열이 입력될 경우, Number()가 10진수로 변환해서 리턴한다.
+    // 예를들어, '0x33'이 입력될 경우 Number('0x33')은 51을 리턴한다. 그럼 true를 리턴하게된다.
+    // 따라서 for문을 이용해서 문자열의 문자 하나하나에 접근해서 확인해줘야한다.
+    
     if(s.length !== 4 && s.length !== 6) return false;
     
     for(ele of s) {
