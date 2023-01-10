633783d93cb165f8b6089b30a4aac99f594e4080
hyunho <61953759+hyunhoh@users.noreply.github.com>
2023-01-10 20:04:25 +0900

    [level 1] Title: 직사각형 별찍기, Time: 52.82 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,19 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    
+    let result = '';
+    let row = '';
+    
+    for(i=0; i<a; i++) {
+        row += '*';
+    }
+    
+    for(i=0; i<b; i++) {
+        result = result + row + '\n';
+    }
+    
+    console.log(result)
+    return result;
+});
