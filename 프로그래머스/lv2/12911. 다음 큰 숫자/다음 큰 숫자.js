793c7ec5a21c4be66992a8e546d8caa0c7af6151
Henu <61953759+hyunhoh@users.noreply.github.com>
2023-01-22 16:53:06 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let ans = n + 1;
+    
+    while(true) {
+        if(ans.toString(2).match(/1/g).length === n.toString(2).match(/1/g).length) {
+            return ans;
+        } else {
+            ans++;
+        }
+    }
+}
