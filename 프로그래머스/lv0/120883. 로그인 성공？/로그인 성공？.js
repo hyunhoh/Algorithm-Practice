860c89d89e1cc8e166185f6401fdd4276734ab4b
Henu <61953759+hyunhoh@users.noreply.github.com>
2023-02-01 00:20:32 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,11 +1,6 @@
 function solution(id_pw, db) {
     for(i=0; i<db.length; i++) {
-        if(db[i][0] === id_pw[0]) {
-            if(db[i][1] === id_pw[1]) {
-                return 'login';
-            }
-            return 'wrong pw'
-        }
+        if(db[i][0] === id_pw[0]) return db[i][1] === id_pw[1] ? 'login' : 'wrong pw'
     }
     return 'fail';
 }
