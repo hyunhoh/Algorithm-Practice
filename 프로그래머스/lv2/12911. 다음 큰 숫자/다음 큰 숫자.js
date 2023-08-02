6fadef06b019186614bccf241120bf5a49bd0fed
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-02 15:29:20 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -2,10 +2,7 @@ function solution(n) {
     let ans = n + 1;
     
     while(true) {
-        if(ans.toString(2).match(/1/g).length === n.toString(2).match(/1/g).length) {
-            return ans;
-        } else {
-            ans++;
-        }
+        if(ans.toString(2).match(/1/g).length === n.toString(2).match(/1/g).length) return ans;
+        ans++;
     }
 }
