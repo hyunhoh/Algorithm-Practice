5a78b95f6218a99e63e1cb5747eb3942b1ce8eae
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-03 23:27:32 +0900

    [unrated] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    const result = [];
+    
+    for(let i=1; i<=my_string.length; i++) {
+        result.push(my_string.slice(-i))
+    }
+    
+    return result.sort();
+}
