48a54834cbb33938c451298700df12af4b5d7cad
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-04 18:22:06 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,8 +1,10 @@
 function solution(arr) {
-    for(let i=0; i<arr.length; i++) {
-        for(let j=0; j<arr[i].length; j++) {
-            if(arr[i][j] !== arr[j][i]) return 0;
-        }
-    }
-    return 1;
+    // for(let i=0; i<arr.length; i++) {
+    //     for(let j=0; j<arr[i].length; j++) {
+    //         if(arr[i][j] !== arr[j][i]) return 0;
+    //     }
+    // }
+    // return 1;
+    
+    return arr.every((e, i) => e.every((e, j) => e === arr[j][i])) ? 1 : 0
 }
