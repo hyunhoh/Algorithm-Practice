6d38c5f4412fe349b033566dc54e2c95822c5114
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-04 12:23:41 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,3 +1,3 @@
 function solution(num_list) {
-    return Math.max(num_list.reduce((acc, cur, idx) => idx % 2 === 0 ? acc + cur : acc, 0), num_list.reduce((acc, cur, idx) => idx % 2 === 1 ? acc + cur : acc, 0))
+    return Math.max(num_list.reduce((acc, cur, idx) => !(idx % 2) ? acc + cur : acc, 0), num_list.reduce((acc, cur, idx) => idx % 2 ? acc + cur : acc, 0))
 }
