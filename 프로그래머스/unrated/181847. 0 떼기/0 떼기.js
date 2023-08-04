5c8e1115ff682df4706e932386e98adb1d48ddee
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-04 18:02:26 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,3 +1,9 @@
 function solution(n_str) {
-    return n_str[0] === '0' ? solution(n_str.slice(1)) : n_str
+    if(n_str[0] === '0') {
+        return solution(n_str.slice(1))
+    } else {
+        return n_str
+    }
+    
+    // return n_str[0] === '0' ? solution(n_str.slice(1)) : n_str
 }
