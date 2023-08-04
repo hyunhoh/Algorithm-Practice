541ccd57d1a12e8885b15a67ceea267d4c54a812
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-04 12:28:54 +0900

    [unrated] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -2,7 +2,7 @@ function solution(str1, str2) {
     let result = '';
     
     for(let i=0; i<str1.length; i++) {
-        result = result + str1[i] + str2[i]
+        result += str1[i] + str2[i]
     }
     
     return result;
