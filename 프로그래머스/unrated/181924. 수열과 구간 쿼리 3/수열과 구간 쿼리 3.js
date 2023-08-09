6d48bff68f8b0c3ca51cd5b210825bca8862fce0
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-09 13:15:48 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.34 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,8 +1,6 @@
 function solution(arr, queries) {
     return queries.reduce((acc, cur) => {
-        const temp = acc[cur[0]];
-        acc[cur[0]] = acc[cur[1]];
-        acc[cur[1]] = temp;
+        [acc[cur[1]], acc[cur[0]]] = [acc[cur[0]], acc[cur[1]]]
         return acc;
     }, arr)
 }
