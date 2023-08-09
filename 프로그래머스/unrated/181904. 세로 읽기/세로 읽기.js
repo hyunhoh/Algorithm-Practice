81cdb329082adaaeed7c463e541c44defc6bab2b
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-09 13:07:23 +0900

    [unrated] Title: 세로 읽기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,9 +1,11 @@
 function solution(my_string, m, c) {
-    const arr = [];
+//     const arr = [];
     
-    for(let i=0; i<my_string.length; i+=m) {
-        arr.push(my_string.slice(i, i+m));
-    }
+//     for(let i=0; i<my_string.length; i+=m) {
+//         arr.push(my_string.slice(i, i+m));
+//     }
     
-    return arr.map(e => e[c-1]).join('')
+//     return arr.map(e => e[c-1]).join('')
+    
+    return [...my_string].filter((_, i) => i % m === c - 1).join('')
 }
