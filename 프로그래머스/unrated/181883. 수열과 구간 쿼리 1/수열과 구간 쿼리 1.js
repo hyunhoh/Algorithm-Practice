ba5e70a2f30afa99ab22fa4545596f93ef3c7e9f
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-09 12:02:11 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 1.74 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,7 +1,7 @@
 function solution(arr, queries) {
     return queries.reduce((acc, cur) => {
         for(let i=cur[0]; i<=cur[1]; i++) {
-            acc[i] = acc[i] + 1;
+            acc[i] += 1;
         }
         return acc;
     }, arr)
