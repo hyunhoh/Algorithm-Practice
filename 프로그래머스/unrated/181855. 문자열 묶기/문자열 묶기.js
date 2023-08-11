2079dd565bf63f460f2ac0d7d4cdc958b83a7582
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-11 19:36:41 +0900

    [unrated] Title: 문자열 묶기, Time: 14.66 ms, Memory: 57.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,11 @@
+function solution(strArr) {
+    const obj = strArr.reduce((acc, cur) => acc[cur.length] ? {...acc, [cur.length]: acc[cur.length] + 1} : {...acc, [cur.length]: 1}, {})
+    
+    let max = 0;
+    
+    for(key in obj) {
+        if(obj[key] > max) max = obj[key]
+    }
+    
+    return max;
+}
