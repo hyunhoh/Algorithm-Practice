bcedb2e0ed4c06c02b75be72f16a5ffafff3deb6
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-16 17:58:42 +0900

    Time: 56 ms (77.22%), Space: 42.5 MB (52.79%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+
+    if(s.length % 2) return false;
+
+    for(l of s) {
+        if(l === '(' || l === '[' || l === '{') {
+            stack.push(l);
+        } else {
+            if(l === ')' && stack[stack.length-1] === '('
+            || l === ']' && stack[stack.length-1] === '['
+            || l === '}' && stack[stack.length-1] === '{') {
+                stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    return !stack.length
+};
