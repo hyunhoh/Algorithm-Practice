b9d8ad387cb8de9b316fe9cec69cecfa54db89bc
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-16 15:28:19 +0900

    Time: 94 ms (43.84%), Space: 41.6 MB (97.02%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i=0; i<nums.length; i++) {
+        if(nums.includes(target - nums[i], i+1)) {
+            return [i, nums.indexOf(target - nums[i], i+1)]
+        }
+    }
+};
