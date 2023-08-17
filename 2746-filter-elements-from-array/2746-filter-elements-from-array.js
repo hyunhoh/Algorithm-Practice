741fda4ecc655cbbbab3d4ce391822f9bef3e4f8
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-17 14:49:38 +0900

    Time: 49 ms (85.32%), Space: 41.8 MB (57.19%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter((e, i) => fn(e, i))
+    const filteredArr = [];
+
+    for(let i=0; i<arr.length; i++) {
+        if(fn(arr[i], i)) filteredArr.push(arr[i]) 
+    }
+
+    return filteredArr;
 };
