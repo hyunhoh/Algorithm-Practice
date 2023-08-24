5b67139ae14fce64caa9875d6c20846f42b401eb
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-08-24 18:59:14 +0900

    Time: 45 ms (93.1%), Space: 41.7 MB (68.63%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,18 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle)
+
+    for(let i=0; i<haystack.length; i++) {
+        if(haystack[i] === needle[0]) {
+            for(let j=0; j<needle.length; j++) {
+                if(haystack[i+j] !== needle[j]) {
+                    break;
+                } else if(j === needle.length - 1) {
+                    return i;
+                }
+            }
+        }
+    }
+
+    return -1;
 };
