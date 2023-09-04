785b1bee3bd773596831ba3149fa8fa7a04e984a
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-09-04 17:55:42 +0900

    Time: 51 ms (70.29%), Space: 42.1 MB (20.69%) - LeetHub

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let step = 0;
+
+    while(num !== 0) {
+        num % 2 ? num-- : num = num / 2 
+        step++;
+    }
+
+    return step
+};
