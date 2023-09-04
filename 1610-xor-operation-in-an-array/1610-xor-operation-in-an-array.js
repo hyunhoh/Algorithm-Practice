121ba065be713c961d1129295eb819d3a04c8c8f
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-09-04 16:52:02 +0900

    Time: 62 ms (10.29%), Space: 41.9 MB (34.8%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+var xorOperation = function(n, start) {
+    return new Array(n).fill(n).map((_, i) => start + 2 * i).reduce((acc, cur) => acc ^ cur);
+};
