2c0105b6fde2b5b243aa5aee4113ab59bb267521
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-09-05 17:05:25 +0900

    Time: 50 ms (73.97%), Space: 42 MB (32.88%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.js
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.js
@@ -3,17 +3,18 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-    let matches = 0;
+    // let matches = 0;
 
-    while(n > 1) {
-        if(n % 2) {
-            matches += Math.floor(n / 2);
-            n = Math.floor(n / 2) + 1;
-        } else {
-            matches += (n / 2);
-            n = n / 2;
-        }
-    }
+    // while(n > 1) {
+    //     if(n % 2) {
+    //         matches += Math.floor(n / 2);
+    //         n = Math.floor(n / 2) + 1;
+    //     } else {
+    //         matches += (n / 2);
+    //         n = n / 2;
+    //     }
+    // }
 
-    return matches;
+    // return matches;
+    return n-1;
 };
