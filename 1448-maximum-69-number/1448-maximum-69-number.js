962a88162feb7cb655ee75a7d8a4effaf55385a1
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-09-07 17:26:06 +0900

    Time: 52 ms (66.38%), Space: 41.6 MB (75.86%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.js
+++ 1448-maximum-69-number/1448-maximum-69-number.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 var maximum69Number  = function(num) {
-    const arr = [...String(num)];
+    // const arr = [...String(num)];
 
-    for(let i=0; i<arr.length; i++) {
-        if(arr[i] === '6') {
-            arr[i] = '9';
-            return Number(arr.join(''))
-        }
-    }
+    // for(let i=0; i<arr.length; i++) {
+    //     if(arr[i] === '6') {
+    //         arr[i] = '9';
+    //         return Number(arr.join(''))
+    //     }
+    // }
 
-    return num;
+    // return num;
+
+    return +String(num).replace('6', '9')
 };
