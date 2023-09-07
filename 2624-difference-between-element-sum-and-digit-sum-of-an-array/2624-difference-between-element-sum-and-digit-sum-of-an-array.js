c3d6d82ebe756086d75ffdc7fdba0e4019bb125c
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-09-07 14:22:09 +0900

    Time: 65 ms (56.19%), Space: 44.9 MB (59.52%) - LeetHub

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.js
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.js
@@ -4,7 +4,7 @@
  */
 var differenceOfSum = function(nums) {
     const eleSum = nums.reduce((acc, cur) => acc + cur, 0)
-    const digitSum = nums.reduce((acc, cur) => acc + [...String(cur)].reduce((acc, cur) => acc + +cur, 0), 0)
+    const digitSum = nums.join('').split('').reduce((acc, cur) => acc + +cur, 0)
 
     return Math.abs(eleSum - digitSum)
 };
