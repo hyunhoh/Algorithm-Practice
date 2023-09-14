16ba2ae879db31631ebeb868d965d6a8f2a31fbf
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-09-14 17:06:56 +0900

    Time: 60 ms (29.25%), Space: 44.2 MB (10.38%) - LeetHub

---
+++ 2401-count-asterisks/2401-count-asterisks.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countAsterisks = function(s) {
+    let pair = true;
+    let count = 0;
+
+    for(char of s) {
+        if(char === '|') {
+            pair = !pair
+        }
+        if(pair) {
+            if(char === '*') count++;
+        }
+    }
+
+    return count;
+};
