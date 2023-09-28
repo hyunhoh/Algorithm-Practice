6d6c67aba3ea35de21653cd2f1167cae43c61c3a
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-09-29 08:48:05 +0900

    Time: 59 ms (40.59%), Space: 44 MB (23.27%) - LeetHub

---
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var pivotInteger = function(n) {
+    let x = 1
+
+    while(x <= n) {
+        if((x ** 2 * 2) === (n ** 2 + n)) return x
+        x++
+    }
+
+    return -1
+};
