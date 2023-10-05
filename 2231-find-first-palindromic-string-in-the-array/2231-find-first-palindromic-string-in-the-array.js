4443536ef4afd04663aafb158157a8617dbc0fd5
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-10-05 12:20:38 +0900

    Time: 54 ms (94.11%), Space: 46.5 MB (81.73%) - LeetHub

---
+++ 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function(words) {
+    for(word of words) {
+        for(let i=0, j=word.length-1; ; i++, j--) {
+            if(word[i] !== word[j]) {
+                break
+            }
+            if(i >= j) return word
+        }
+    }
+
+    return ''
+};
