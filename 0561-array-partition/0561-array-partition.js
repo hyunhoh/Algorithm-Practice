e3e379bbcc76c174cd2f9c6ffd3f734fb1fefa34
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-10-10 18:54:53 +0900

    Time: 96 ms (77.93%), Space: 47 MB (79.63%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -3,5 +3,5 @@
  * @return {number}
  */
 var arrayPairSum = function(nums) {
-    return nums.sort((a,b) => a-b).filter((e, i) => i % 2 === 0).reduce((acc, cur) => acc + cur, 0)
+    return nums.sort((a,b) => b-a).reduce((acc, cur, idx) => idx % 2 ? acc + cur : acc, 0)
 };
