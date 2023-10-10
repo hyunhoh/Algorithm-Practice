83932e600da0da2986f6dd652ae82b4f81f98580
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-10-10 16:14:17 +0900

    Time: 54 ms (88.89%), Space: 44.4 MB (76.39%) - LeetHub

---
+++ 2819-remove-trailing-zeros-from-a-string/2819-remove-trailing-zeros-from-a-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var removeTrailingZeros = function(num) {
+    for(let i=num.length-1; i>=0; i--) {
+        if(num[i] !== '0') return num.slice(0, i+1)   
+    }
+};
