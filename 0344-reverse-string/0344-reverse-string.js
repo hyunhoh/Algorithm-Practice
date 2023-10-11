8badda73391a6fb233086b16618ecf786426a4ec
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-10-11 17:29:49 +0900

    Time: 82 ms (40.73%), Space: 49.1 MB (72.21%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -4,9 +4,7 @@
  */
 var reverseString = function(s) {
     for(let i=0, j=s.length-1; i<j; i++, j--) {
-        let temp = s[i]
-        s[i] = s[j]
-        s[j] = temp
+        [s[i], s[j]]= [s[j], s[i]]
     }
 
     return s
