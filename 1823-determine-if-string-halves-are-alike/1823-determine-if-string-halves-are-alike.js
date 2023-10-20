3d8f8444af916a447a2265f502b33f701957e299
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-10-20 20:49:36 +0900

    Time: 61 ms (30.49%), Space: 43 MB (43.09%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.js
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.js
@@ -4,13 +4,12 @@
  */
 var halvesAreAlike = function(s) {
     const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    let a = 0
-    let b = 0
+    let count = 0
 
     for(let i=0, j=s.length-1; i<j; i++, j--) {
-        if(vowels.includes(s[i])) a+=1
-        if(vowels.includes(s[j])) b+=1
+        if(vowels.includes(s[i])) count+=1
+        if(vowels.includes(s[j])) count-=1
     }
 
-    return a === b
+    return count === 0
 };
