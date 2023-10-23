abf26f26f2cd571ca4e200b32ec7ed0f32e78ab4
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-10-23 12:12:27 +0900

    Time: 47 ms (84.26%), Space: 41 MB (96.45%) - LeetHub

--- 2733-sleep/2733-sleep.js
+++ 2733-sleep/2733-sleep.js
@@ -1,12 +1,9 @@
 /**
  * @param {number} millis
+ * @return {Promise}
  */
 async function sleep(millis) {
-    return new Promise((res) => {
-        setTimeout(() => {
-            res();
-        }, millis)
-    })
+    return new Promise(res => setTimeout(res, millis))
 }
 
 /** 
