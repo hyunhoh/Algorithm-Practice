6fb3c512036d2af3061ada5ec54068b40bda9ddd
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-10-27 09:41:40 +0900

    Time: 73 ms (22.29%), Space: 45 MB (20.71%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.js
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.js
@@ -3,19 +3,22 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    for(let i=0, j=nums.length-1; i<j;) {
-        if(nums[i] % 2 === 0) {
-            i++
-        } else if(nums[j] % 2 === 1) {
-            j--
-        } else {
-        [nums[i], nums[j]] = [nums[j], nums[i]]
-        i++
-        j--
 
+    for(let i=0, j=nums.length-1; i<j;) {
+
+        if(!(nums[i] % 2)) {
+            i++  
         }
 
+        else if(nums[j] % 2) {
+            j--
+        }
         
+        else {
+            [nums[i], nums[j]] = [nums[j], nums[i]]
+            i++
+            j--
+        }
     }
 
     return nums
