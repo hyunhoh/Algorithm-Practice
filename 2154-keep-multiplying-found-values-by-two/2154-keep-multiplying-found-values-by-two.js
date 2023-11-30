6c4185efee935c5bf3f2a56ef98af96fc3df5860
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2023-11-30 13:45:34 +0900

    Time: 55 ms (46.90%), Space: 42.1 MB (84.51%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} original
+ * @return {number}
+ */
+var findFinalValue = function(nums, original) {
+    while(true) {
+        if(nums.includes(original)) {
+            original *= 2
+        } else {
+            return original
+        }
+    }
+};
