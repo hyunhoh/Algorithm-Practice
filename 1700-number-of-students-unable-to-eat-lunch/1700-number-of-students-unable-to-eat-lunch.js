ed0b6aef3b880821b89c662f72e993b3dac40d6b
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2024-01-12 20:05:23 +0900

    Time: 53 ms (48.21%), Space: 42.1 MB (43.11%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function(students, sandwiches) {
+    while(students.includes(sandwiches[0])) {
+        if(students[0] === sandwiches[0]) {
+            students.shift()
+            sandwiches.shift()
+        } else {
+            students.push(students.shift())
+        }
+    }
+
+    return students.length
+};
