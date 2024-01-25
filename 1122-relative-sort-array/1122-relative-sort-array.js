d3f82c4bb41082b36888fe7ad36f2caf6951260c
HYUNHO HAN <61953759+hyunhoh@users.noreply.github.com>
2024-01-25 20:14:47 +0900

    Time: 63 ms (22.54%), Space: 50.4 MB (5.33%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.js
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -4,9 +4,9 @@
  * @return {number[]}
  */
 var relativeSortArray = function(arr1, arr2) {
-    const front = arr2.map(e2 => arr1.filter(e1 => e1 === e2))
+    const front = arr2.flatMap(e2 => arr1.filter(e1 => e1 === e2))
 
     const rear = arr1.filter(e => !arr2.includes(e)).sort((a,b) => a-b)
 
-    return [...front, ...rear].flat()
+    return [...front, ...rear]
 };
